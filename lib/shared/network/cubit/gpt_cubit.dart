f9b18826be8a1312131b3a3002830ef379c13d0c
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-09-20 21:39:46 +0300

    This commit removes the file lib/shared/network/end_points.dart✅

---
+++ lib/shared/network/cubit/gpt_cubit.dart
@@ -0,0 +1,7 @@
+import 'package:chat_gpt/shared/network/cubit/gpt_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class GptCubit extends Cubit<GptState> {
+  GptCubit() : super(GptInitialState());
+  static GptCubit get(context) => BlocProvider.of(context);
+}
---
+++ lib/shared/network/cubit/gpt_state.dart
@@ -0,0 +1,3 @@
+abstract class GptState {}
+
+class GptInitialState extends GptState {}
