946618bbaa05aa15388129798f53ad187a121901
HusseinMohamed99 <husseinhtm99@gmail.com>
2023-09-21 16:37:34 +0300

    Remove bloc, cubit, cache and dio packages and their related code ✅

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:chat_gpt/screen/OnBoarding/on_boarding_screen.dart';
-import 'package:chat_gpt/shared/network/cache_helper.dart';
-import 'package:chat_gpt/shared/network/dio_helper.dart';
+import 'package:chat_gpt/shared/providers/chats_provider.dart';
+import 'package:chat_gpt/shared/providers/models_provider.dart';
 import 'package:chat_gpt/shared/style/enum/enum.dart';
 import 'package:chat_gpt/shared/style/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   ScreenUtil.ensureScreenSize();
-  DioHelper.init();
-  await CacheHelper.init();
 
   runApp(const MyApp());
 }
@@ -20,19 +19,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: ScreenUtil.defaultSize,
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (context, child) {
-          return MaterialApp(
-            title: 'Chat GPT',
-            theme: getThemeData[AppTheme.lightTheme],
-            darkTheme: getThemeData[AppTheme.lightTheme],
-            themeMode: ThemeMode.light,
-            debugShowCheckedModeBanner: false,
-            home: const OnBoardingScreen(),
-          );
-        });
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ModelsProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(),
+        ),
+      ],
+      child: ScreenUtilInit(
+          designSize: ScreenUtil.defaultSize,
+          minTextAdapt: true,
+          splitScreenMode: true,
+          builder: (context, child) {
+            return MaterialApp(
+              title: 'Chat GPT',
+              theme: getThemeData[AppTheme.lightTheme],
+              darkTheme: getThemeData[AppTheme.lightTheme],
+              themeMode: ThemeMode.light,
+              debugShowCheckedModeBanner: false,
+              home: const OnBoardingScreen(),
+            );
+          }),
+    );
   }
 }
